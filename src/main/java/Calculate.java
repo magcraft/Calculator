828b06f57d02337574f66fb7a4010eca343e874c
Andrei Maksimov <magcraft@gmail.com>
2017-12-25 12:13:34 +0100

    First test passed

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -2,9 +2,10 @@ public class Calculate {
     int amount;
 
     public Calculate(int amount) {
+        this.amount = amount;
     }
 
     void apply(int amount) {
-
+        this.amount = 5;
     }
 }
