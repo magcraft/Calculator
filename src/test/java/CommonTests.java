f663af2beea0ff19b58b532d67d020a8c94fe27f
Andrei Maksimov <magcraft@gmail.com>
2017-12-25 14:45:59 +0100

    tests refactored: each test check only one case

--- src/test/java/CommonTests.java
+++ src/test/java/CommonTests.java
@@ -9,29 +9,58 @@ public class CommonTests {
         Calculate calculate = new Calculate();
         calculate.apply(5);
         assertEquals(5, calculate.amount);
-        assertFalse(calculate.apply(6));
+    }
+
+    @Test
+    public void applyTwiceTest() {
+        Calculate calculate = new Calculate();
+        calculate.apply(5);
+        calculate.apply(6);
         assertEquals(5, calculate.amount);
+    }
+
+    @Test
+    public void secondApplyWillFailTest() {
+        Calculate calculate = new Calculate();
+        calculate.apply(5);
+        assertFalse(calculate.apply(6));
     }
 
     @Test
     public void addTest() {
         Calculate calculate = new Calculate();
-        assertFalse(calculate.add(4));
-        assertEquals(0, calculate.amount);
-        assertFalse(calculate.add(5));
-        assertEquals(0, calculate.amount);
         assertTrue(calculate.apply(0));
         assertTrue(calculate.add(2));
         assertEquals(2, calculate.amount);
     }
 
     @Test
+    public void addBeforeApplyTest() {
+        Calculate calculate = new Calculate();
+        assertFalse(calculate.add(4));
+        assertEquals(0, calculate.amount);
+    }
+
+
+    @Test
     public void divideTest() {
         Calculate calculate = new Calculate();
-        calculate.apply(0);
-        calculate.add(6);
-        assertFalse(calculate.divide(0));
+        calculate.apply(6);
         calculate.divide(2);
         assertEquals(3, calculate.amount);
     }
+
+    @Test
+    public void divideBeforeApplyTest() {
+        Calculate calculate = new Calculate();
+        assertFalse(calculate.divide(5));
+    }
+
+    @Test
+    public void divideByZeroTest() {
+        Calculate calculate = new Calculate();
+        calculate.apply(6);
+        assertFalse(calculate.divide(0));
+    }
+
 }
