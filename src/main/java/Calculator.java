ee73d70a739bf1848dffe031dcff0efee0a70faa
Andrei Maksimov <magcraft@gmail.com>
2017-12-26 18:55:49 +0100

    add renamed files

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,34 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.exists;
+
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length == 1) {
+            String argument = args[0];
+            Path sourcePath = Paths.get(argument);
+            if (!exists(sourcePath)) {
+                try {
+                    System.out.println(String.format("File: '%s' does not exists", argument));
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                System.exit(0);
+            }
+            FileHandler fileHandler = new FileHandler(sourcePath.toString());
+            RunCommands runCommands = new RunCommands(new Calculate(), fileHandler.readFile());
+
+            int result;
+            result = runCommands.runCommands();
+            if (result == -1) {
+                System.out.println(String.format("File: %s contains an error", argument));
+            } else {
+                System.out.println(result);
+            }
+        } else {
+            System.out.println("Argument is missing");
+            System.out.println(printHelpMessage.getMessage());
+        }
+    }
+}
